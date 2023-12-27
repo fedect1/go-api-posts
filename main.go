69a0c569a3f8596991ba212b614e04bf237c27f6
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-12-27 09:56:06 +0000

    Add new files and update existing files

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	lambda "github.com/aws/aws-lambda-go/lambda"
 	"github.com/fedect1/go-api-posts/awsgo"
+	"github.com/fedect1/go-api-posts/bd"
+	"github.com/fedect1/go-api-posts/handlers"
+	"github.com/fedect1/go-api-posts/models"
 	"github.com/fedect1/go-api-posts/secretmanager"
 )
 
@@ -41,7 +45,43 @@ func ejecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		}
 		return res, nil	
 	}
+
+	path := strings.Replace(request.PathParameters["postApp"], os.Getenv("UrlPrefix"), "", -1)
 	
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.Username)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSign"), SecretModel.JWTSign)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
+
+	err = bd.ConectarDB(awsgo.Ctx)
+	if err != nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body: "Error conectando la DB "+err.Error(),
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil	
+	}
+	restAPI := handlers.Manejadores(awsgo.Ctx, request)
+	if restAPI.CustomResp == nil {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: restAPI.Status,
+			Body: restAPI.Message,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil	
+	} else {
+		return restAPI.CustomResp, nil
+	}
 }
 
 func ValidoParametros() bool {
--- models/sm.go
+++ models/sm.go
@@ -4,6 +4,6 @@ type Secret struct {
 	Host string `json:"host"`
 	Username string `json:"username"`
 	Password string `json:"password"`
-	JWTSing string `json:"jwtsign"`
+	JWTSign string `json:"jwtsign"`
 	Database string `json:"database"`
 }
